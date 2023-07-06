1519962506298a28f3cefaf9a885eb850c522216
Diego Lopes <dieolpes77@gmail.com>
2023-07-05 22:57:00 -0300

    feito - vetores

--- vector.py
+++ vector.py
@@ -1,50 +1,63 @@
 """Módulo com as funções de manipulação de vetores."""
 
-
+import math
 def norma(x: list[float]) -> float | None:
     """Calcula a norma de um vetor"""
-    # TODO: implementar
-    # a norma de um vetor [3, 4] é 5
-    # a norma de um vetor [1, 2, 4] é 4.58257569495584
-    # ela consiste em calcular a raiz quadrada da soma dos quadrados dos elementos do vetor
-    # se o vetor estiver vazio retorne None
+    if not x:
+        return None
+    soma_quadrados = sum([elem ** 2 for elem in x])
+    norma = math.sqrt(soma_quadrados)
+    return norma
 
 
 def soma(x: list[float], y: list[float]) -> list[float] | None:
     """Soma dois vetores"""
     # TODO: implementar
-    # a soma de dois vetores [1, 2, 4] + [2, 3, 4] é [3, 5, 8]
-    # a soma só pode ser realizada se os vetores tem a mesma quantidade de elementos.
-    # caso contrário, deve retornar None
+    if len(x) != len(y):
+        return None
+
+    resultado = [xi + yi for xi, yi in zip(x, y)]
+    return resultado
 
 
 def multiplicação_por_escalar(vetor: list[float], escalar: float) -> list[float]:
     """Multiplica um vetor por um escalar"""
     # TODO: implementar
-    # a multiplicação de um vetor [1, 2, 4] por um escalar 2 é [2, 4, 8]
-
+    resultado = [elemento * escalar for elemento in vetor]
+    return resultado
 
 def produto_interno(x: list[float], y: list[float]) -> float | None:
     """Calcula o produto interno de dois vetores"""
     # TODO: implementar
-    # o produto interno de dois vetores [1, 2, 4] e [2, 3, 4] é 24
-    # o produto interno consiste em multiplicar os elementos de mesmo índice e somar os resultados
-    # a multiplicação só pode ser realizada se os vetores tem a mesma quantidade de elementos.
-    # caso contrário, deve retornar None
-    # caso os vetores sejam vazios o resultado é 0
+    if len(x) != len(y):
+        return None
+
+    if not x or not y:
+        return 0
+
+    resultado = sum([xi * yi for xi, yi in zip(x, y)])
+    return resultado
 
 
 def produto_vetorial(x: list[float], y: list[float]) -> list[float] | None:
     """Calcula o produto vetorial de dois vetores"""
     # TODO: implementar
-    # o produto vetorial de dois vetores [1, 2, 4] e [2, 3, 4] é [-4, 4, -1]
-    # o produto vetorial só pode ser realizado se os vetores tem 3 elementos.
-    # caso contrário, deve retornar None
+    if len(x) != 3 or len(y) != 3:
+        return None
+    resultado = [
+        x[1] * y[2] - x[2] * y[1],
+        x[2] * y[0] - x[0] * y[2],
+        x[0] * y[1] - x[1] * y[0]
+    ]
+
+    return resultado
 
 
 def produto_diádico(x: list[float], y: list[float]) -> list[list[float]] | None:
     """Calcula o produto diádico de dois vetores"""
     # TODO: implementar
-    # o produto diádico de dois vetores [1, 2, 4] e [2, 3, 4] é [[2, 3, 4], [4, 6, 8], [8, 12, 16]]
-    # o produto diádico só pode ser realizado se os vetores tem a mesma quantidade de elementos.
-    # caso contrário, deve retornar None
+    if len(x) != len(y):
+        return None
+
+    produto = [[xi * yj for yj in y] for xi in x]
+    return produto
